5cb9f306476550ebbd202f4882b4a880e7ee16ba
jollen <jollen@moko365.com>
2014-08-15 16:38:08 +0800

    use NSURLConnectionDelegate

--- HeyWeather/ViewController.swift
+++ HeyWeather/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
