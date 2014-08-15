b0281ce4785e58bcdc6bae705c14c573ba823a8b
jollen <jollen@moko365.com>
2014-08-15 17:20:22 +0800

    call REST API

--- HeyWeather/ViewController.swift
+++ HeyWeather/ViewController.swift
@@ -13,11 +13,22 @@ class ViewController: UIViewController, NSURLConnectionDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        startConnection()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func startConnection() {
+        var restAPI: String = "http://api.openweathermap.org/data/2.5/weather?q=Taipei"
+        var url: NSURL = NSURL(string: restAPI)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)
+        
+        println("start download")
     }
 
     // -- 
